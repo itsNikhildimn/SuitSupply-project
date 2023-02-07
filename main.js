e06fa5e8ecd373abeceeebd664c31f9ed7a486b3
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-02-07 13:33:08 +0530

    last-merge

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// animation-section3
 const view =document.querySelector(".expandview");
 window.addEventListener("scroll",()=>{
     if (window.pageYOffset>1350){
@@ -5,3 +6,22 @@ window.addEventListener("scroll",()=>{
     }
        
 })
+// //animation-section3
+
+
+const cta = document.querySelectorAll(".categories");
+const items = document.querySelector(".foot-Links");
+for(i=0;i<cta.length;i++){
+    cta[i].addEventListener("click",()=>{
+    this.classList.toggle("active");
+    });
+}
+
+
+// FOR CHATBOX
+const openChatBox = document.querySelector(".faq-icon")
+const chatBox = document.querySelector(".pop-up");
+openChatBox.addEventListener("click",()=>{
+chatBox.classList.toggle('active')
+openChatBox.classList.toggle('active')
+});
