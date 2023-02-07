d611b50096541ae6326314909a2f90ce4a80a0b8
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-02-07 13:46:39 +0530

    header-added

--- main.js
+++ main.js
@@ -25,3 +25,50 @@ openChatBox.addEventListener("click",()=>{
 chatBox.classList.toggle('active')
 openChatBox.classList.toggle('active')
 });
+
+
+
+// Hamburger menu //
+const menuBar = document.querySelector(".icon-container");
+const slide = document.querySelector(".header-nav");
+
+menuBar.addEventListener('click',()=>{
+    slide.classList.add('active');
+});
+
+const main = document.querySelector("main")
+
+main.addEventListener('click',()=>{
+    slide.classList.remove('active');
+});
+// Hamburger menu //
+
+
+// side menu //
+const arrow = document.querySelector("#arrow");
+const rightslide = document.querySelector(".right-slider")
+arrow.addEventListener('click',()=>{
+    rightslide.classList.add('active');
+
+})
+
+const arrow2 = document.querySelector("#shoes");
+const rightslide2 = document.querySelector(".right-slider.shoes");
+arrow2.addEventListener('click',()=>{
+    rightslide2.classList.add('active');
+
+})
+
+const arrow3 = document.querySelector("#accessories");
+const rightslide3 = document.querySelector(".right-slider.accessories");
+arrow3.addEventListener('click',()=>{
+    rightslide3.classList.add('active');
+
+})
+
+// const searchbar = document.querySelector(".srch");
+// const divactive = document.querySelector(".search-bar");
+// searchbar.addEventListener('click',()=>{
+//     divactive.classList.add('active');
+// })
+// side menu //
