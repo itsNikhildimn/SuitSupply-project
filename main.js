c9d2cea2469a46fb60e2d8cf1e0b26bc08433a8c
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-02-13 18:40:40 +0530

    adding anchor tag in blogg section

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 //<<<<<<<<<<<----------HEADER JS FUNCTIONS---------------->>>>>>>>>>>>>>>>>>
 
+// TEXT SLIDER
+const textSlide = document.querySelector(".text-slide");
+const text = textSlide.querySelectorAll("a");
+
+let counter = 1;
+const size = text[0].clientWidth;
+
+//to set the slide one as the first slide instead of the clone
+textSlide.style.transform = `translateX(-${size * counter}px)`;
+
+//looping the slider every 2s
+setInterval(()=>{
+  if(counter >= text.length -1) return;
+  
+  textSlide.style.transition = `transform 0.8s ease`;
+  counter++;
+  textSlide.style.transform = `translateX(-${size * counter}px)`; 
+  
+}, 7000);
+
+textSlide.addEventListener('transitionend', () => {
+  if(text[counter].id === 'lastClone'){
+    textSlide.style.transition = `none`;
+    counter = text.length - 2;
+    textSlide.style.transform = `translateX(-${size * counter}px)`;
+  }
+  
+  if(text[counter].id === 'firstClone'){
+    textSlide.style.transition = `none`;
+    counter = text.length - counter;
+    textSlide.style.transform = `translateX(-${size * counter}px)`;
+  }  
+});
+
+// TEXT SLIDER
+
+
+
 // HEADER OFF-SET FUNCTION
 (function(){
 
