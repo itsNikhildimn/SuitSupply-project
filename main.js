4015b13b46cd1e6b1b5ae104215e080a189fa3a2
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-02-15 20:37:46 +0530

    merge single product page

--- main.js
+++ main.js
@@ -212,17 +212,17 @@ for(i=0; i<accordionProduct.length; i++){
 
 
 
-const toClick = document.querySelector('.left-arrow');
-const mask = document.querySelector('.mask')
-toClick.addEventListener("click", () =>{
-    mask.classList.add('active')
-});
+// const toClick = document.querySelector('.left-arrow');
+// const mask = document.querySelector('.mask')
+// toClick.addEventListener("click", () =>{
+//     mask.classList.add('active')
+// });
 
 
-const toClickright = document.querySelector('.right-arrow');
- toClickright.addEventListener('click',()=>{
-        mask.classList.remove('active')
-    })
+// const toClickright = document.querySelector('.right-arrow');
+//  toClickright.addEventListener('click',()=>{
+//         mask.classList.remove('active')
+//     })
  
 
 
@@ -230,12 +230,12 @@ const toClickright = document.querySelector('.right-arrow');
 
 
 // FOR POP-UP CHATBOX
-// const openChatBox = document.querySelector(".faq-icon")
-// const chatBox = document.querySelector(".pop-up");
-// openChatBox.addEventListener("click",()=>{
-//     chatBox.classList.toggle('active')
-//     openChatBox.classList.toggle('active')
-// });
+const openChatBox = document.querySelector(".faq-icon")
+const chatBox = document.querySelector(".pop-up");
+openChatBox.addEventListener("click",()=>{
+    chatBox.classList.toggle('active')
+    openChatBox.classList.toggle('active')
+});
 // /FOR POP-UP CHATBOX
 
 //<<<<<<<<<<<----------FOOTER JS FUNCTIONS ENDS---------------->>>>>>>>>>>>>>>>>>
